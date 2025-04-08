044a7463672154343c142178be638bdce9fc1ac8
Arthur Sena <139017819+ASsena@users.noreply.github.com>
2025-04-08 10:41:06 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
+// Importa a biblioteca axios, que é usada para fazer requisições HTTP
 import axios from "axios";
 
+// Cria uma instância do axios com uma URL base pré-definida.
+// Essa URL será usada como base para todas as requisições feitas com essa instância.
+// Ou seja, você só precisa informar o endpoint complementar ao fazer a requisição.
 const api = axios.create({
     baseURL : 'https://rickandmortyapi.com/api/character'
 })
 
+// Exporta a instância do axios criada para que ela possa ser utilizada em outros arquivos do projeto
 export default api;
